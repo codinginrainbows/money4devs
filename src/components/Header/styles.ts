40792117ca4a66625c62f7f0ea1c3e8a3d0f063d
codinginrainbows <gabrielmgarcia96@gmail.com>
2021-11-30 16:07:39 -0300

    header created

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--blue);
+
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+
+  margin: 0 auto;
+  padding: 2rem 1rem 12rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    cursor: pointer;
+  }
+
+  button {
+    background: var(--blue-light);
+    font-weight: 600;
+    font-size: 1rem;
+    color: #fff;
+    padding: 0 2rem;
+    height: 3rem;
+    border: 0;
+    border-radius: 0.25rem;
+
+    transition: transform 0.1s, filter 0.1s;
+
+    &:hover {
+      filter: brightness(0.9);
+      transform: scale(1.04);
+    }
+  }
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB2;
+
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+    --green: #33CC95;
+    --red: #E52E4D;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;  //15px (padrão 16px)
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;   //14px (padrão 16px)
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, button, textarea, input {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
